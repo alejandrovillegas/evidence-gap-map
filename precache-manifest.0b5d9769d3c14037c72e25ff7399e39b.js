7165b0fb37ac792294f178145767170c6af35685
Alejandro Villegas <neopilatos@gmail.com>
2020-05-12 15:36:38 -0500

    Updates

---
+++ precache-manifest.0b5d9769d3c14037c72e25ff7399e39b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "438bb26f387f073e5702c0b38ce89872",
+    "url": "/evidence-gap-map/index.html"
+  },
+  {
+    "revision": "d515e9bd87957f3734c5",
+    "url": "/evidence-gap-map/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8218a83678f4806223f7",
+    "url": "/evidence-gap-map/static/js/2.3fe980b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/evidence-gap-map/static/js/2.3fe980b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d515e9bd87957f3734c5",
+    "url": "/evidence-gap-map/static/js/main.2cc69c2f.chunk.js"
+  },
+  {
+    "revision": "a9962b7c35e93db0dedf",
+    "url": "/evidence-gap-map/static/js/runtime-main.37e9dc87.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/evidence-gap-map/static/media/logo.5d5d9eef.svg"
+  }
+]);
